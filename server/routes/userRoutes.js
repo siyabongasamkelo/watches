95b8f16fe98d03de7ae74b23336744223593f316
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-02 13:29:19 -0800

    feat: added delete user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,10 +4,12 @@ import { registerUser } from "../controllers/userController.js";
 import { loginUser } from "../controllers/userController.js";
 import { getUser } from "../controllers/userController.js";
 import { getAllUsers } from "../controllers/userController.js";
+import { deleteUser } from "../controllers/userController.js";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/get/:userId", getUser);
 router.get("/get", getAllUsers);
+router.delete("/delete/:userId", deleteUser);
 
 export default router;
