3cc30f2e885be4844f0258aa91ce382d264836a4
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-02 12:53:05 -0800

    feat: add getUser route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 import { registerUser } from "../controllers/userController.js";
 import { loginUser } from "../controllers/userController.js";
+import { getUser } from "../controllers/userController.js";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/get/:userId", getUser);
 
 export default router;
