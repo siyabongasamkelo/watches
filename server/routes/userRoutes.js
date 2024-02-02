d47f63dc5ad7ac60a1e3f0e2d46ca25e7c18f323
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-02 12:35:53 -0800

    feat: added login router

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { registerUser } from "../controllers/userController.js";
+import { loginUser } from "../controllers/userController.js";
 
-router.post("/user", registerUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
-// module.exports = router;
 export default router;
