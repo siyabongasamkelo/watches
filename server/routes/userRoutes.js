f5abd6d3106b8d21f4aac0942060661da3aa0bb9
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-02 13:22:01 -0800

    fea: added get all users route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import { registerUser } from "../controllers/userController.js";
 import { loginUser } from "../controllers/userController.js";
 import { getUser } from "../controllers/userController.js";
+import { getAllUsers } from "../controllers/userController.js";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/get/:userId", getUser);
+router.get("/get", getAllUsers);
 
 export default router;
