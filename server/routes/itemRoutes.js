143e4baba061e58e3eb6f63101ec6f2482b128d8
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-02 17:33:58 -0800

    feat: added update & delete routes

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { createItem } from "../controllers/itemController.js";
+import {
+  createItem,
+  deleteItem,
+  updateItem,
+} from "../controllers/itemController.js";
 import { getItem } from "../controllers/itemController.js";
+import { getItems } from "../controllers/itemController.js";
 
 const router = express.Router();
 
 router.post("/create", createItem);
-router.get("/get", getItem);
+router.get("/get/:itemId", getItem);
+router.get("/get", getItems);
+router.put("/update/:itemId", updateItem);
+router.delete("/delete/:itemId", deleteItem);
 
 export default router;
