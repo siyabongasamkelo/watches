a4ac4cf23d1d9776f2f87594de72dce34014b1e6
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-04 15:51:10 -0800

    feat: added delee review route

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -5,10 +5,12 @@ import {
   createReview,
   getReview,
   updateReview,
+  deleteReview,
 } from "../controllers/reviewController.js";
 
 router.post("/create", createReview);
 router.get("/get", getReview);
 router.put("/update/:reviewId", updateReview);
+router.delete("/delete/:reviewId", deleteReview);
 
 export default router;
