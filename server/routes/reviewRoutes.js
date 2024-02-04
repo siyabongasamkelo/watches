0772bfb5c8d4cc6afc37eea28d34d9f762d0c543
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-04 15:46:22 -0800

    feat: added update router

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { createReview, getReview } from "../controllers/reviewController.js";
+import {
+  createReview,
+  getReview,
+  updateReview,
+} from "../controllers/reviewController.js";
 
 router.post("/create", createReview);
 router.get("/get", getReview);
+router.put("/update/:reviewId", updateReview);
 
 export default router;
