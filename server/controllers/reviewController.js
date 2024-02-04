b93de330c041a62f14b05b383ba741ccb7afd323
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-04 15:44:44 -0800

    feat: created update review function

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -34,10 +34,21 @@ const getReview = async (req, res) => {
 
 const updateReview = async (req, res) => {
   try {
+    const { reviewId } = req.params;
+    const { review, rating, itemId, userId } = req.body;
+
+    if (!reviewId || !review || !rating || !itemId || !userId)
+      return res.status(400).json("all feilds are required");
+
+    const update = { review, rating, itemId, userId };
+    const reviewUpdate = await reviewModel.findByIdAndUpdate(reviewId, update, {
+      new: true,
+    });
+    res.status(200).json("review updated successfully");
   } catch (err) {
     console.log(err);
     res.json(400).json(err);
   }
 };
 
-export { createReview, getReview };
+export { createReview, getReview, updateReview };
