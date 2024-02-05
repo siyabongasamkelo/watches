5d9b1d78b5ae298e72c6ca808b77cf05924dbb11
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-05 12:52:23 -0800

    header element

--- client/watches/src/components/header/Header.js
+++ client/watches/src/components/header/Header.js
@@ -1,7 +1,41 @@
-import { HeaderStyled } from "./Header styled";
+import {
+  CartTotal,
+  HeaderStyled,
+  Logo,
+  LogoAndSearch,
+  MenuAndCart,
+} from "./Header.styled";
+import { Container } from "react-bootstrap";
+import { Search, List, BagFill } from "react-bootstrap-icons";
+import logo from "../../assets/images/logo.png";
 
 const Header = () => {
-  return <HeaderStyled></HeaderStyled>;
+  return (
+    <Container>
+      <HeaderStyled>
+        <LogoAndSearch>
+          <Logo>
+            <img src={logo} alt="logo" />
+          </Logo>
+          <Search
+            style={{
+              marginRight: "30px",
+              //   transform: "scale(1.3)"
+            }}
+          />
+        </LogoAndSearch>
+        <MenuAndCart>
+          <CartTotal>R 4000.00</CartTotal>
+          <BagFill
+          // style={{ transform: "scale(1.3)" }}
+          />
+          <List
+          // style={{ transform: "scale(1.4)" }}
+          />
+        </MenuAndCart>
+      </HeaderStyled>
+    </Container>
+  );
 };
 
 export default Header;
