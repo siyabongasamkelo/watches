f455f663af47a03c4cfbd0d38235e288bb17956d
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-06 11:01:23 -0800

    feat: created routing system & added theme provider

--- client/watches/src/App.js
+++ client/watches/src/App.js
@@ -1,11 +1,44 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const theme = {
+    light: {
+      primary: "#ac7d88;",
+      secondary: " #fdf0d1",
+      text: "rgba(0,0,0,0.7)",
+      smallText: "rgba(0, 0, 0, 0.8)",
+    },
+    size: {
+      small: "13px",
+      medium: "17px",
+      large: "22px",
+      extraLarge: "32px",
+    },
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/*" element={<HomePage />} />
+      </Route>
+    )
+  );
   return (
-    <div>
-      <HomePage />
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
   );
 }
 
