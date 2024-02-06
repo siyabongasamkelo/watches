4334e02c7e66c6d044a4f225b222d691e3cbbbcc
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-06 11:04:46 -0800

    fix: made header fully responsive on all devices

--- client/watches/src/components/home/Home.styled.js
+++ client/watches/src/components/home/Home.styled.js
@@ -5,20 +5,14 @@ export const Homestyled = styled.section`
   font-family: "Kalam", cursive;
   height: 100vh;
   width: 100%;
-  @media only screen and (max-width: 600px) {
-  }
 `;
 
 export const HeroSection = styled.div`
-  @media only screen and (max-width: 600px) {
-    height: 85%;
-    width: 100%;
-    position: relative;
-  }
-  @media only screen and (min-width: 768px) {
-    height: 85%;
-    width: 100%;
-    position: relative;
+  height: 85%;
+  width: 100%;
+  position: relative;
+  @media only screen and (min-width: 1200px) {
+    display: flex;
   }
 `;
 
@@ -63,6 +57,26 @@ export const HeroImages = styled.div`
       left: 51%;
     }
   }
+  @media only screen and (min-width: 1200px) {
+    height: 100%;
+    width: 50%;
+    .pic1 {
+      height: 43%;
+      width: 23%;
+      border-radius: 10px;
+      position: absolute;
+      left: 0%;
+      top: 10%;
+    }
+    .pic2 {
+      height: 33%;
+      width: 17%;
+      border-radius: 10px;
+      position: absolute;
+      top: 42%;
+      left: 25%;
+    }
+  }
 `;
 
 export const HeroText = styled.div`
@@ -76,19 +90,29 @@ export const HeroText = styled.div`
     width: 60%;
     margin-left: 20%;
   }
+  @media only screen and (min-width: 1200px) {
+    height: 100%;
+    width: 35%;
+  }
 `;
 
 export const HeroHeader = styled.h1`
-  color: #ac7d88;
+  color: ${(props) => props.theme.light.primary};
   text-align: center;
   @media only screen and (max-width: 600px) {
+  }
+  @media only screen and (min-width: 1200px) {
+    padding-top: 15%;
+    font-size: 52px;
   }
 `;
 
 export const HeroParagraph = styled.p`
-  color: rgba(0, 0, 0, 0.8);
+  color: ${(props) => props.theme.light.smallText};
   text-align: center;
-  @media only screen and (max-width: 600px) {
+  @media only screen and (min-width: 1200px) {
+    text-align: left;
+    font-size: 17px;
   }
 `;
 
@@ -97,15 +121,37 @@ export const HeroAction = styled.div`
   margin-top: 20%;
   display: flex;
   justify-content: center;
-  @media only screen and (max-width: 600px) {
+  @media only screen and (min-width: 1200px) {
+    justify-content: left;
+    margin-top: 8%;
+  }
+`;
+
+export const SocialMediaIcons = styled.div`
+  height: 60px;
+  width: 150px;
+  margin-top: 70%;
+  display: none;
+  @media only screen and (min-width: 1200px) {
+    display: block;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    svg {
+      transform: scale(1.6);
+      cursor: pointer;
+      fill: ${(props) => props.theme.light.primary};
+      transition: 0.5s ease-in-out;
+      &:hover {
+        transform: scale(1.7);
+      }
+    }
   }
 `;
 
 export const MyButton = styled.button`
   height: 40px;
   min-width: 180px;
-  background-color: #fdf0d1;
+  background-color: ${(props) => props.theme.light.secondary};
   border-radius: 30px;
-  @media only screen and (max-width: 600px) {
-  }
 `;
