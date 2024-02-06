2b4e9f8dbc534fa4801b30850080f23a096cab17
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-06 14:25:28 -0800

    feat: added categories on homePage

--- client/watches/src/pages/HomePage.js
+++ client/watches/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "../components/home/Home";
+import Categories from "../components/categories/Categories";
 
 const HomePage = () => {
   return (
     <div>
       <Home />
+      <Categories />
     </div>
   );
 };
