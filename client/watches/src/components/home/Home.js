a360648fd186b9d0ffd8bfb631d3f3f1f8371252
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-05 17:04:49 -0800

    feat: creating hero section & whole home page

--- client/watches/src/components/home/Home.js
+++ client/watches/src/components/home/Home.js
@@ -1,11 +1,41 @@
+import { Container } from "react-bootstrap";
 import Header from "../header/Header";
-import { Homestyled } from "./Home.styled";
+import {
+  HeroAction,
+  HeroHeader,
+  HeroImages,
+  HeroParagraph,
+  HeroSection,
+  HeroText,
+  Homestyled,
+  MyButton,
+} from "./Home.styled";
+import watch1 from "../../assets/images/hero1.jpg";
+import watch2 from "../../assets/images/hero2.jpg";
 
 const Home = () => {
   return (
-    <Homestyled>
-      <Header />
-    </Homestyled>
+    <Container>
+      <Homestyled>
+        <Header />
+        <HeroSection>
+          <HeroImages>
+            <img src={watch1} alt="watch" className="pic1" />
+            <img src={watch2} alt="watch" className="pic2" />
+          </HeroImages>
+          <HeroText>
+            <HeroHeader>Luxurious Watches</HeroHeader>
+            <HeroParagraph>
+              These are the world class watches trust me nothing is better than
+              this you gotta trus me...
+            </HeroParagraph>
+            <HeroAction>
+              <MyButton>Shop Now</MyButton>
+            </HeroAction>
+          </HeroText>
+        </HeroSection>
+      </Homestyled>
+    </Container>
   );
 };
 
