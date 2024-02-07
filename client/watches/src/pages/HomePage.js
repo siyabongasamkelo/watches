923d2660311d43a6c79e3a7b9e42914c5a0d23e8
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-07 11:07:33 -0800

    feat: added PopularProducts

--- client/watches/src/pages/HomePage.js
+++ client/watches/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "../components/home/Home";
 import Categories from "../components/categories/Categories";
+import PopularProducts from "../components/popularProducts/PopularProducts";
 
 const HomePage = () => {
   return (
     <div>
       <Home />
       <Categories />
+      <PopularProducts />
     </div>
   );
 };
