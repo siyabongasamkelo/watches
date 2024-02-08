598bb7f66393b1527d34f9c1e929ec4d3c175437
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-08 13:50:15 -0800

    feat: HeaderSlider styles

---
+++ client/watches/src/components/header/HeaderSlider.styled.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { HeroHeader } from "../home/Home.styled";
+import { GetHelpItems, SubscribeInput } from "../footer/Footer.styled";
+
+export const HeaderSliderStyled = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: white;
+  position: absolute;
+  z-index: 1;
+  top: 0%;
+  border-radius: 10px 0 0 10px;
+  overflow-x: hidden;
+  @media only screen and (min-width: 992px) {
+    display: none;
+  }
+`;
+
+export const CloseButton = styled.div`
+  height: 40px;
+  margin-bottom: 20%;
+  svg {
+    fill: rgba(0, 0, 0, 0.8);
+    transform: scale(3);
+    margin: 30px;
+  }
+`;
+
+export const HeaderSliderItems = styled(HeroHeader)`
+  margin-top: 5%;
+`;
+
+export const HeaderSliderSearch = styled(SubscribeInput)`
+  margin-top: 10%;
+  margin-left: 20%;
+`;
+
+export const Copyrights = styled(GetHelpItems)`
+  text-align: center;
+  margin-top: 10%;
+`;
