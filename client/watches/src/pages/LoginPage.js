cacbcb8c6d39a3a648e63c80d1f8d2abbaf35a26
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-08 17:29:29 -0800

    feat: login page with email & password textbox

---
+++ client/watches/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import Login from "../components/login/Login";
+import { Container } from "react-bootstrap";
+const LoginPage = () => {
+  return (
+    <Container>
+      <Login />
+    </Container>
+  );
+};
+
+export default LoginPage;
