51240c42ae89cfe78461936e2c503ea6fb520083
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-08 17:50:56 -0800

    chore: folder for all register components

---
+++ client/watches/src/components/register/Register.js
@@ -0,0 +1,7 @@
+import { RegisterStyled } from "./Register.styled";
+
+const Register = () => {
+  return <RegisterStyled></RegisterStyled>;
+};
+
+export default Register;
---
+++ client/watches/src/components/register/Register.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const RegisterStyled = styled.section`
+  width: 100%;
+  background-color: red;
+`;
