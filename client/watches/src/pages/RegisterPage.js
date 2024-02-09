a7bf8445e87396ff31e88138937310e05b1015f2
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-08 18:58:47 -0800

    register page

---
+++ client/watches/src/pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import Register from "../components/register/Register";
+import Footer from "../components/footer/Footer";
+const RegisterPage = () => {
+  return (
+    <Container>
+      <Register />
+      <Footer />
+    </Container>
+  );
+};
+
+export default RegisterPage;
