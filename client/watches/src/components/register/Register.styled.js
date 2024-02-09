86a731d428c8b8e8fc2eecc2b4e0ff50ab041a93
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-08 18:58:14 -0800

    feat: all styles for register page

--- client/watches/src/components/register/Register.styled.js
+++ client/watches/src/components/register/Register.styled.js
@@ -1,6 +1,17 @@
 import styled from "styled-components";
+import { LoginForm, LoginHeader, LoginInput } from "../login/Login.Styled";
 
 export const RegisterStyled = styled.section`
   width: 100%;
-  background-color: red;
 `;
+
+export const RegisterContent = styled.div`
+  height: 70vh;
+  width: 100%;
+`;
+
+export const RegisterHeader = styled(LoginHeader)``;
+
+export const RegisterForm = styled(LoginForm)``;
+
+export const RegisterInput = styled(LoginInput)``;
