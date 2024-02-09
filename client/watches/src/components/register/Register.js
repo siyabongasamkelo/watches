86620d02978ecb7294e711f664c9f2f35f1b0e6c
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-08 18:57:45 -0800

    feat: created register page

--- client/watches/src/components/register/Register.js
+++ client/watches/src/components/register/Register.js
@@ -1,7 +1,29 @@
-import { RegisterStyled } from "./Register.styled";
+import Header from "../header/Header";
+import { SubmitButton } from "../login/Login.Styled";
+import {
+  RegisterContent,
+  RegisterForm,
+  RegisterHeader,
+  RegisterInput,
+  RegisterStyled,
+} from "./Register.styled";
 
 const Register = () => {
-  return <RegisterStyled></RegisterStyled>;
+  return (
+    <RegisterStyled>
+      <Header />
+      <RegisterContent>
+        <RegisterHeader>Register</RegisterHeader>
+        <RegisterForm>
+          <RegisterInput placeholder="type username..." />
+          <RegisterInput placeholder="type email..." />
+          <RegisterInput placeholder="type password..." />
+          <RegisterInput placeholder="confirm password..." />
+          <SubmitButton>Submit</SubmitButton>
+        </RegisterForm>
+      </RegisterContent>
+    </RegisterStyled>
+  );
 };
 
 export default Register;
