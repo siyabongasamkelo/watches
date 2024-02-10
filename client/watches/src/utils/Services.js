c13dd19da9cf12ae2341bb00801d12026f986e7a
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-10 10:53:01 -0800

    fix: fixed postRequrst to indicated if there's an error

--- client/watches/src/utils/Services.js
+++ client/watches/src/utils/Services.js
@@ -2,36 +2,17 @@ import axios from "axios";
 export const baseUrl = "http://localhost:5000";
 
 export const postRequest = async (url, body) => {
-  const response = await axios.post(url, body);
+  try {
+    const response = await axios.post(url, body);
 
-  console.log(response);
-  const data = await response.json();
-
-  if (!response.ok) {
-    let message;
-
-    if (data?.message) {
-      message = data.message;
-    } else {
-      message = data;
-    }
-
-    return { error: true, message };
+    return { data: response, error: false };
+  } catch (err) {
+    return { err, error: true };
   }
-
-  return data;
 };
 
 export const getRequest = async (url) => {
   const response = await axios.get(url);
-  const data = await response.json();
-  if (!response.ok) {
-    let message = data.message;
 
-    if (data?.message) {
-      message = data.message;
-    }
-    return { error: true, message };
-  }
-  return data;
+  return response;
 };
