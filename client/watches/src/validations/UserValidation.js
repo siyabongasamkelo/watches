9daa63832ce9b489db0b3bdb0b4de964b876e4bf
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-10 10:53:57 -0800

    fix: increased passord length

--- client/watches/src/validations/UserValidation.js
+++ client/watches/src/validations/UserValidation.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 export const userSchema = yup.object().shape({
   username: yup.string().required(),
   email: yup.string().email("email not valid").required(),
-  password: yup.string().min(3).max(8).required(),
+  password: yup.string().min(3).max(14).required(),
   conPassword: yup
     .string()
     .oneOf([yup.ref("password"), null], "passwords must match")
