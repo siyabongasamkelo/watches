5698fe8ae444cb19d5664440ff2b3de83872eaeb
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-13 14:52:25 -0800

    feat: component/page for displaying all the shop items

---
+++ client/watches/src/pages/ShopPage.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import Shop from "../components/shop/Shop";
+
+const ShopPage = () => {
+  return (
+    <Container>
+      <Shop />
+    </Container>
+  );
+};
+
+export default ShopPage;
