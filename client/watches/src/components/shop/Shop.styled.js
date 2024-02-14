a78b6c074ca069e47cbb91a9264e5e0a2fff3e19
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-13 16:07:16 -0800

    feat: added filturing components

--- client/watches/src/components/shop/Shop.styled.js
+++ client/watches/src/components/shop/Shop.styled.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 import { TextBox } from "../header/Header.styled";
+import { GetHelpHeader, GetHelpItems } from "../footer/Footer.styled";
 
 export const ShopStyled = styled.section`
   width: 100%;
+  font-family: "Great Vibes", cursive;
+  font-family: "Kalam", cursive;
 `;
 
 export const ShopContent = styled.div`
@@ -13,7 +16,7 @@ export const ShopContent = styled.div`
 export const FilterAndSort = styled.div`
   height: 100%;
   width: 30%;
-  background-color: red;
+  padding: 3%;
 `;
 
 export const ShopItems = styled.div`
@@ -22,4 +25,20 @@ export const ShopItems = styled.div`
   width: 70%;
 `;
 
-export const SearchItem = styled(TextBox)``;
+export const SearchItem = styled(TextBox)`
+  margin-top: 10%;
+  width: 100%;
+`;
+
+export const FilterHeader = styled(GetHelpHeader)`
+  margin-top: 10%;
+  text-align: left;
+`;
+
+export const CategoriesHeader = styled(FilterHeader)`
+  margin-top: 25%;
+`;
+
+export const CategoriesItem = styled(GetHelpItems)`
+  color: rgba(0, 0, 0, 0.7);
+`;
