74ca56da9d5ab0b8c6fbdbe9138de8144040a213
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-14 09:26:59 -0800

    feat: added footer to the page

--- client/watches/src/pages/ShopPage.js
+++ client/watches/src/pages/ShopPage.js
@@ -1,10 +1,12 @@
 import { Container } from "react-bootstrap";
 import Shop from "../components/shop/Shop";
+import Footer from "../components/footer/Footer";
 
 const ShopPage = () => {
   return (
     <Container>
       <Shop />
+      <Footer />
     </Container>
   );
 };
