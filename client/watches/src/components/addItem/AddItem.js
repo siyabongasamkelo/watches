529265d4cf2404a513b8043ea5a15f5b70858a31
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-14 17:43:19 -0800

    chore: add items folder

---
+++ client/watches/src/components/addItem/AddItem.js
@@ -0,0 +1,158 @@
+import Header from "../header/Header";
+import {
+  AddItemContent,
+  AddItemForm,
+  AddItemHeader,
+  AddItemInput,
+  AddItemStyled,
+} from "./AddItem.styled";
+import { useFormik } from "formik";
+import { baseUrl, postRequest } from "../../utils/Services";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import { itemSchema } from "../../validations/ItemValidation";
+import { ErrorLabel } from "../login/Login.Styled";
+
+const showToastErrorMessage = (message) => {
+  toast.error(message);
+};
+
+const successToastMessage = (message) => {
+  toast.success(message);
+};
+const AddItem = () => {
+  const [loading, setLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      price: "",
+      category: "",
+      quantity: "",
+      description: "",
+      image: null,
+    },
+    validationSchema: itemSchema,
+    onSubmit: async () => {
+      setLoading(true);
+      try {
+        const { name } = formik.values;
+        const { price } = formik.values;
+        const { category } = formik.values;
+        const { image } = formik.values;
+        const { description } = formik.values;
+        const { quantity } = formik.values;
+
+        const formData = new FormData();
+        formData.append("image", image);
+        formData.append("name", name);
+        formData.append("price", price);
+        formData.append("category", category);
+        formData.append("description", description);
+        formData.append("quantity", quantity);
+
+        const loggingUser = await postRequest(
+          `${baseUrl}/item/create`,
+          formData
+        );
+
+        if (loggingUser?.error) {
+          showToastErrorMessage("there was a problem while adding your item");
+          setLoading(false);
+          return showToastErrorMessage(loggingUser?.err?.response?.data);
+        }
+
+        setLoading(false);
+        successToastMessage("item added successfully");
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+        showToastErrorMessage("Something went wrong please try again later");
+      }
+    },
+  });
+
+  return (
+    <AddItemStyled>
+      <Header />
+      <ToastContainer />
+      <AddItemContent>
+        <AddItemHeader>Add item</AddItemHeader>
+        <AddItemForm onSubmit={formik.handleSubmit}>
+          <div>
+            <AddItemInput
+              placeholder="item name..."
+              name="name"
+              value={formik.values.name}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik?.errors && <ErrorLabel>{formik?.errors?.name}</ErrorLabel>}
+          </div>
+          <div>
+            <AddItemInput
+              placeholder="item price..."
+              name="price"
+              value={formik.values.price}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik?.errors && <ErrorLabel>{formik?.errors?.price}</ErrorLabel>}
+          </div>
+
+          <div>
+            <AddItemInput
+              placeholder="item category..."
+              name="category"
+              value={formik.values.category}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik?.errors && (
+              <ErrorLabel>{formik?.errors?.category}</ErrorLabel>
+            )}
+          </div>
+
+          <div>
+            <AddItemInput
+              placeholder="item description..."
+              name="description"
+              value={formik.values.description}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik?.errors && (
+              <ErrorLabel>{formik?.errors?.description}</ErrorLabel>
+            )}
+          </div>
+
+          <div>
+            <AddItemInput
+              placeholder="item quantity..."
+              name="quantity"
+              value={formik.values.quantity}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik?.errors && (
+              <ErrorLabel>{formik?.errors?.quantity}</ErrorLabel>
+            )}
+          </div>
+
+          <div>
+            <input
+              type="file"
+              name="image"
+              onChange={(e) => {
+                formik.setFieldValue("image", e.target.files[0]);
+              }}
+            />
+            {formik?.errors && <ErrorLabel>{formik?.errors?.image}</ErrorLabel>}
+          </div>
+        </AddItemForm>
+      </AddItemContent>
+    </AddItemStyled>
+  );
+};
+
+export default AddItem;
---
+++ client/watches/src/components/addItem/AddItem.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import {
+  LoginContent,
+  LoginForm,
+  LoginHeader,
+  LoginInput,
+} from "../login/Login.Styled";
+
+export const AddItemStyled = styled.section`
+  height: 70vh;
+`;
+
+export const AddItemContent = styled(LoginContent)``;
+
+export const AddItemHeader = styled(LoginHeader)`
+  margin-top: 5%;
+`;
+
+export const AddItemForm = styled(LoginForm)``;
+
+export const AddItemInput = styled(LoginInput)``;
