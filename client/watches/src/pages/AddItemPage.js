e04eab483cded8e1de06c9bdca6ca531f43a81a2
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-14 17:47:09 -0800

    feat: added addItem component & footer

---
+++ client/watches/src/pages/AddItemPage.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import AddItem from "../components/addItem/AddItem";
+import Footer from "../components/footer/Footer";
+const AddItemPage = () => {
+  return (
+    <Container>
+      <AddItem />
+      <Footer />
+    </Container>
+  );
+};
+
+export default AddItemPage;
