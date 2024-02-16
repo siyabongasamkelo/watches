18536973a000c37953cd0362b7935f053f5fd5b1
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-15 17:59:43 -0800

    fix: added rating & time stamps

--- server/model/itemModel.js
+++ server/model/itemModel.js
@@ -1,37 +1,45 @@
 import mongoose from "mongoose";
 
-const itemSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 100,
+const itemSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: [String],
+      required: true,
+      minLength: 3,
+      maxLength: 40,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+    },
+    image: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 100,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: [String],
-    required: true,
-    minLength: 3,
-    maxLength: 40,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 100,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const itemModel = mongoose.model("Item", itemSchema);
 export { itemModel };
