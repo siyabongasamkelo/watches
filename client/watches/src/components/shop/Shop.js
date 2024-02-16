98acc40682be2bdc62948a53415a8ad04c5cee73
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-16 14:20:21 -0800

    feat: made filturing sysytem work perfectly now trying to intergrate pagination

--- client/watches/src/components/shop/Shop.js
+++ client/watches/src/components/shop/Shop.js
@@ -12,57 +12,57 @@ import {
   ShopStyled,
   SortBy,
 } from "./Shop.styled";
-import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
+import Slider from "react-slider";
 import Form from "react-bootstrap/Form";
 import ProductCard from "../ProductCard";
-import classic from "../../assets/images/c1.jpg";
-import minimalist from "../../assets/images/m1.jpg";
-import minimalist2 from "../../assets/images/m2.jpg";
-import advanced from "../../assets/images/a1.jpg";
 import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
-import { baseUrl, getRequest } from "../../utils/Services";
+import { baseUrl, currencyFormatter, getRequest } from "../../utils/Services";
 import { toast, ToastContainer } from "react-toastify";
+import Pagination from "../Pagination";
 
 const showToastErrorMessage = (message) => {
   toast.error(message);
 };
 
-// const successToastMessage = (message) => {
-//   toast.success(message);
-// };
 const Shop = () => {
-  const [sliderValues, setSliderValues] = useState([0, 100]); // Initial slider values
+  const [values, setValues] = useState([0, 10000]);
+  const handleChange = (newValues) => setValues(newValues);
+
   const [search, setSearch] = useState("");
   const [sort, setSort] = useState("");
   const [category, setCategory] = useState("All");
   const [page, setPage] = useState(1);
 
-  const handleSliderChange = (newValues) => {
-    setSliderValues(newValues);
-  };
-
   const fetchItems = async () => {
     try {
       const items = await getRequest(
-        `${baseUrl}/item/get?page=${page}&limit=6&search=${search}&sort=${sort}&category=${category}`
+        `${baseUrl}/item/get?page=${page}&limit=6&search=${search}&sort=${sort}&category=${category}&minPrice=${values[0]}&maxPrice=${values[1]}`
       );
 
-      return items.data;
+      // return items?.data?.items;
+      return items?.data;
     } catch (err) {
       console.log(err);
       showToastErrorMessage("there was a problem while fetching items");
     }
   };
 
+  let minPrice = currencyFormatter.format(values[0]);
+  let maxPrice = currencyFormatter.format(values[1]);
+
   const queryKey = ["items", { sort, category, search, page }];
   const { data, status, refetch } = useQuery(queryKey, fetchItems);
-  console.log(data);
 
   useEffect(() => {
     refetch();
-  }, [sort, category, search, page, refetch]);
+  }, [sort, category, search, page, refetch, values, setValues]);
+
+  const handlePageChange = (page) => {
+    // Fetch paginated data from the server based on the selected page
+    // Update the UI with the new data
+  };
 
   return (
     <ShopStyled>
@@ -77,13 +77,26 @@ const Shop = () => {
             }}
           />
           <FilterHeader>Filter by price</FilterHeader>
-          <RangeSlider
+
+          <Slider
+            className="slider"
+            value={values}
+            onChange={handleChange}
             min={0}
-            max={100}
-            values={sliderValues}
-            onChange={handleSliderChange}
+            max={10000}
           />
+
+          <p>min : {minPrice}</p>
+          <p>max : {maxPrice}</p>
+
           <CategoriesHeader>Product categories</CategoriesHeader>
+          <CategoriesItem
+            onClick={() => {
+              setCategory("All");
+            }}
+          >
+            All
+          </CategoriesItem>
           <CategoriesItem
             onClick={() => {
               setCategory("classic");
@@ -119,18 +132,17 @@ const Shop = () => {
             </Form.Select>
           </SortBy>
           <ItemContainer>
-            {/* <ProductCard image={advanced} isSpaceSmall={true} />
-            <ProductCard image={classic} isSpaceSmall={true} />
-            <ProductCard image={minimalist} isSpaceSmall={true} />
-            <ProductCard image={minimalist2} isSpaceSmall={true} />
-            <ProductCard image={advanced} isSpaceSmall={true} />
-            <ProductCard image={classic} isSpaceSmall={true} /> */}
-
-            {data?.map((item) => (
+            {data?.items?.map((item) => (
               <ProductCard image={item?.image} isSpaceSmall={true} />
             ))}
           </ItemContainer>
-          <PaginationContainer></PaginationContainer>
+          <PaginationContainer>
+            <Pagination
+              totalItems={data?.items?.length}
+              itemsPerPage={6}
+              onPageChange={data?.page}
+            />
+          </PaginationContainer>
         </ShopItems>
       </ShopContent>
     </ShopStyled>
