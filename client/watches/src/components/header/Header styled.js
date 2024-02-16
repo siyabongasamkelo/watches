006b49cd3cf1b5e16ab2f391a04d538c41a7938e
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-15 18:00:32 -0800

    random temp files

--- client/watches/src/components/header/Header styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-export const HeaderStyled = styled.header`
-  @media only screen and (max-width: 600px) {
-    height: 15vh;
-    width: 100%;
-    background-color: red;
-  }
-`;
