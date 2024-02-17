c6bf269aa65542762191af9fb73974ba4885a718
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-17 13:55:41 -0800

    feat: made page responsive for mobile

--- client/watches/src/components/previewItem/PreviewItemStyled.js
+++ client/watches/src/components/previewItem/PreviewItemStyled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { CartegoryHeader } from "../categories/CartegoryDiv";
 import { GetHelpItems } from "../footer/Footer.styled";
 import { HeroParagraph } from "../home/Home.styled";
-import { ProductPrice } from "../ProductCard";
+import { MyButton, ProductPrice } from "../ProductCard";
 
 export const PreviewStyled = styled.section`
   width: 100%;
@@ -12,22 +12,50 @@ export const PreviewStyled = styled.section`
 
 export const PreviewContainer = styled.div`
   display: flex;
-  margin-top: 150px;
+  flex-direction: column;
+  margin-top: 40px;
+  /* @media only screen and (max-width: 600px) {
+    flex-direction: column;
+  } */
+  @media only screen and (min-width: 992px) {
+    flex-direction: row;
+    margin-top: 150px;
+  }
 `;
 
 export const ImageHolder = styled.div`
-  width: 50%;
+  /* width: 50%;
   height: 90vh;
   img {
     height: 65%;
     width: 90%;
     object-fit: cover;
+  } */
+  width: 100%;
+  height: 50vh;
+  img {
+    height: 100%;
+    width: 100%;
+  }
+  @media only screen and (min-width: 992px) {
+    width: 50%;
+    height: 90vh;
+    img {
+      height: 65%;
+      width: 90%;
+      object-fit: cover;
+    }
   }
 `;
 
 export const ItemDetails = styled.div`
-  width: 50%;
-  height: 90vh;
+  /* width: 50%;
+  height: 90vh; */
+  height: 50vh;
+  @media only screen and (min-width: 992px) {
+    width: 50%;
+    height: 90vh;
+  }
 `;
 
 export const ItemName = styled(CartegoryHeader)`
@@ -46,4 +74,12 @@ export const ItemQuantity = styled.div`
   display: flex;
 `;
 
-export const PreviewParagraph = styled(HeroParagraph)``;
+export const PreviewParagraph = styled(HeroParagraph)`
+  text-align: left;
+`;
+
+export const AddToCart = styled(MyButton)`
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+  }
+`;
