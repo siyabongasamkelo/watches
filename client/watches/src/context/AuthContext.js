ec2ef33a06d0eac09cc87499e7eee98dec790113
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-19 15:56:25 -0800

    test: was testing if users was stored properly in local storage

--- client/watches/src/context/AuthContext.js
+++ client/watches/src/context/AuthContext.js
@@ -1,14 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  // const storedData = localStorage.getItem("User");
+  // const parsedData = JSON.parse(storedData);
+  // console.log("userDATA", parsedData);
+
+  console.log("user", user);
+
+  useEffect(() => {
+    const user = localStorage.getItem("User");
+
+    setUser(JSON.parse(user));
+  }, []);
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
+
   return (
     <AuthContext.Provider
       value={{
         user,
-        setUser,
+        updateUser,
       }}
     >
       {children}
