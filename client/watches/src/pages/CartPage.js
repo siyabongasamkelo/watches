daae9bf894412d6ae2ab3fe5a73aeda7d39acdaa
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-20 13:48:23 -0800

    feat: added cart components to cartpage

---
+++ client/watches/src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+import Cart from "../components/cart/Cart";
+const CartPage = () => {
+  return (
+    <Container>
+      <Cart />
+    </Container>
+  );
+};
+
+export default CartPage;
