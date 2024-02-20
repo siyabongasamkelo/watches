d2281055262f1b61c7bcaaccf6436ed4d756c770
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-20 13:47:35 -0800

    feat: made form responsive to all devices

--- client/watches/src/components/reviews/ReviewForm.styled.js
+++ client/watches/src/components/reviews/ReviewForm.styled.js
@@ -1,21 +1,39 @@
 import styled from "styled-components";
 import { LoginHeader } from "../login/Login.Styled";
 import { TextArea } from "../addItem/AddItem.styled";
+import { TextBox } from "../header/Header.styled";
+import { MyButton } from "../home/Home.styled";
 
 export const ReviewFormStyled = styled.div`
   width: 30%;
-  height: 40vh;
+  /* height: 40vh; */
   margin-top: 20px;
   border-radius: 20px;
   background-color: ${(props) => props.theme.light.tetiary};
   display: flex;
   justify-content: center;
   align-items: center;
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 50%;
+  }
+  @media only screen and (min-width: 992px) {
+    width: 50%;
+  }
+  @media only screen and (min-width: 1200px) {
+    width: 30%;
+  }
 `;
 
 export const ReviewForms = styled.form`
   width: 90%;
   height: 90%;
+`;
+
+export const ReviewInput = styled(TextBox)`
+  display: block;
 `;
 
 export const ReviewFormHeader = styled(LoginHeader)`
@@ -29,8 +47,16 @@ export const ReviewTextArea = styled(TextArea)`
 `;
 
 export const SubmitButtons = styled.div`
-  width: 40%;
+  width: 100%;
   display: flex;
-  margin-top: 30px;
+  margin-top: 20px;
+  margin-bottom: 30px;
   justify-content: space-between;
+  @media only screen and (max-width: 600px) {
+    margin-top: 0;
+  }
+`;
+export const ReviewSubmit = styled(MyButton)`
+  min-width: 100px;
+  /* background-color: red; */
 `;
