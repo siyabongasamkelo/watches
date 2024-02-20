fd3d92f88004e0db1a9ebba9429bd80392558cee
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-20 14:11:19 -0800

    feat: added footer

--- client/watches/src/pages/CartPage.js
+++ client/watches/src/pages/CartPage.js
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
 import Cart from "../components/cart/Cart";
+import Footer from "../components/footer/Footer";
 const CartPage = () => {
   return (
     <Container>
       <Cart />
+      <Footer />
     </Container>
   );
 };
