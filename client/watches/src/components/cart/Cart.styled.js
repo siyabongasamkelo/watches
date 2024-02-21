7953779b121bd526d20ea2400f6dbb52e04a0462
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-21 14:36:04 -0800

    cart styles resposnible for all devices

--- client/watches/src/components/cart/Cart.styled.js
+++ client/watches/src/components/cart/Cart.styled.js
@@ -1,10 +1,41 @@
 import styled from "styled-components";
 import { LoginHeader } from "../login/Login.Styled";
+import { GetHelpHeader } from "../footer/Footer.styled";
+import { Price } from "./CartItem.styled";
+import { MyButton } from "../ProductCard";
+// import { MyButton } from "../home/Home.styled";
 
 export const CartStyled = styled.section`
   font-family: "Great Vibes", cursive;
   font-family: "Kalam", cursive;
 `;
 
-export const CartHeader = styled(LoginHeader)``;
+export const CartHeader = styled(LoginHeader)`
+  margin-top: 5%;
+`;
 export const CartContainer = styled.div``;
+
+export const CartTotalContainer = styled.div``;
+
+export const CartTotalHeader = styled(GetHelpHeader)`
+  margin-top: 3%;
+  margin-bottom: 4%;
+`;
+export const CartTotal = styled(Price)`
+  display: block;
+`;
+
+export const CartSubTotalCover = styled.div`
+  width: 100%;
+  margin-top: 1%;
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (min-width: 992px) {
+    width: 40%;
+  }
+`;
+
+export const CheckoutButton = styled(MyButton)`
+  margin-top: 3%;
+  height: 40px;
+`;
