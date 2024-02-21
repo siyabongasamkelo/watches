d5a3af1c58db7d0a40b8cff23fd01c757a8e1377
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-21 14:35:24 -0800

    feat: cart page layout responsible for all devices but not yet dynamic

--- client/watches/src/components/cart/Cart.js
+++ client/watches/src/components/cart/Cart.js
@@ -1,5 +1,14 @@
 import Header from "../header/Header";
-import { CartContainer, CartHeader, CartStyled } from "./Cart.styled";
+import {
+  CartContainer,
+  CartHeader,
+  CartStyled,
+  CartSubTotalCover,
+  CartTotal,
+  CartTotalContainer,
+  CartTotalHeader,
+  CheckoutButton,
+} from "./Cart.styled";
 import CartItem from "./CartItem";
 
 const Cart = () => {
@@ -11,6 +20,28 @@ const Cart = () => {
         <CartItem />
         <CartItem />
       </CartContainer>
+      <CartTotalContainer>
+        <CartTotalHeader>Cart Total</CartTotalHeader>
+        <CartSubTotalCover>
+          <CartTotal>SUBTOTAL</CartTotal>
+          <CartTotal>
+            <strong>R 3000.00</strong>
+          </CartTotal>
+        </CartSubTotalCover>
+        <CartSubTotalCover>
+          <CartTotal>SHIPPING</CartTotal>
+          <CartTotal>
+            <strong>R 200.00</strong>
+          </CartTotal>
+        </CartSubTotalCover>
+        <CartSubTotalCover>
+          <CartTotal>TOTAL</CartTotal>
+          <CartTotal>
+            <strong>R 3 200.00</strong>
+          </CartTotal>
+        </CartSubTotalCover>
+        <CheckoutButton>Proceed To Checkout</CheckoutButton>
+      </CartTotalContainer>
     </CartStyled>
   );
 };
