3bcfbe2b102d91e018740ddd8c6178550de3a9d7
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-22 13:07:58 -0800

    feat: made it responsive

--- client/watches/src/components/checkout/BillingDetailsForm.styled.js
+++ client/watches/src/components/checkout/BillingDetailsForm.styled.js
@@ -1,25 +1,38 @@
 import styled from "styled-components";
 import { TextBox } from "../header/Header.styled";
 import { CategoriesHeader } from "../categories/Categories";
+import { MyButton } from "../home/Home.styled";
 
-export const BillingStyled = styled.div``;
+export const BillingStyled = styled.div`
+  margin-bottom: 30px;
+`;
 
 export const BillingForm = styled.form`
-  width: 30%;
+  width: 100%;
+  @media only screen and (min-width: 992px) {
+    width: 30%;
+  }
 `;
 
 export const BillingFormCover = styled.div`
   display: flex;
+  flex-direction: column;
+  @media only screen and (min-width: 992px) {
+    flex-direction: row;
+  }
 `;
 
 export const BillingHeader = styled(CategoriesHeader)``;
 
 export const BillingLabel = styled.label`
-  margin-left: 45px;
-  margin-bottom: 10px;
+  margin: 10px 0 10px 45px;
 `;
 
 export const BillingInput = styled(TextBox)`
   display: block;
   margin-left: 30px;
+`;
+
+export const ContinueButton = styled(MyButton)`
+  margin: 30px 0 10px 30px;
 `;
