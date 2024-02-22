96840e2751dadec025b5df4b4aeaae46fce27d09
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-21 18:28:31 -0800

    feat: added formCover and inputs

--- client/watches/src/components/checkout/BillingDetailsForm.js
+++ client/watches/src/components/checkout/BillingDetailsForm.js
@@ -1,17 +1,56 @@
 import {
   BillingForm,
+  BillingFormCover,
+  BillingHeader,
   BillingInput,
   BillingLabel,
   BillingStyled,
 } from "./BillingDetailsForm.styled";
+import { CountryDropdown } from "react-select-country-list";
 
 const BillingDetailsForm = () => {
   return (
     <BillingStyled>
-      <BillingForm>
-        <BillingLabel>First Name</BillingLabel>
-        <BillingInput type="text" />
-      </BillingForm>
+      <BillingHeader>Billing Form</BillingHeader>
+      <BillingFormCover>
+        <BillingForm>
+          <div>
+            <BillingLabel>First Name</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+          <div>
+            <BillingLabel>Last Name</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+          <div>
+            <BillingLabel>Phone</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+          <div>
+            <BillingLabel>Email Address</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+        </BillingForm>
+        <BillingForm>
+          <div>
+            {/* <CountryDropdown
+              value="US" // Set a default value if needed
+            /> */}
+          </div>
+          <div>
+            <BillingLabel>Last Name</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+          <div>
+            <BillingLabel>Phone</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+          <div>
+            <BillingLabel>Email Address</BillingLabel>
+            <BillingInput type="text" />
+          </div>
+        </BillingForm>
+      </BillingFormCover>
     </BillingStyled>
   );
 };
