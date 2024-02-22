54430ecbe7ad1a9d1dec57cfc0b0e2243cf1acf1
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-21 18:29:12 -0800

    feat: all Billing form styles

--- client/watches/src/components/checkout/BillingDetailsForm.styled.js
+++ client/watches/src/components/checkout/BillingDetailsForm.styled.js
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 import { TextBox } from "../header/Header.styled";
+import { CategoriesHeader } from "../categories/Categories";
 
 export const BillingStyled = styled.div``;
 
-export const BillingForm = styled.form``;
+export const BillingForm = styled.form`
+  width: 30%;
+`;
 
-export const BillingLabel = styled.label``;
+export const BillingFormCover = styled.div`
+  display: flex;
+`;
+
+export const BillingHeader = styled(CategoriesHeader)``;
+
+export const BillingLabel = styled.label`
+  margin-left: 45px;
+  margin-bottom: 10px;
+`;
 
 export const BillingInput = styled(TextBox)`
   display: block;
+  margin-left: 30px;
 `;
