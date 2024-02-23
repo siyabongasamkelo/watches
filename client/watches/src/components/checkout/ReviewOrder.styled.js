91fa3f4b6b58d44e7f2c58a8b00f278d49eaf0d1
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-22 18:40:15 -0800

    reviewOrder comp styles

---
+++ client/watches/src/components/checkout/ReviewOrder.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { BillingFormCover, BillingHeader } from "./BillingDetailsForm.styled";
+import {
+  CartSubTotalCover,
+  CartTotal,
+  CartTotalHeader,
+} from "../cart/Cart.styled";
+
+export const ReviewOrderStyled = styled.div``;
+
+export const ReviewOrderHeader = styled(BillingHeader)``;
+
+export const ReviewOrderCover = styled.div``;
+
+export const ReviewTotalHeader = styled(CartTotalHeader)``;
+
+export const ReviewTotalCover = styled(CartSubTotalCover)``;
+
+export const ReviewTotal = styled(CartTotal)``;
+
+export const PaymentMethodCover = styled.div``;
