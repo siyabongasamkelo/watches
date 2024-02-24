5d9ff3e0ec02f3dc6dae3b5a4222e6cba59fedf5
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-23 19:17:49 -0800

    fix: removed id & used _id

--- client/watches/src/context/CartContext.js
+++ client/watches/src/context/CartContext.js
@@ -7,37 +7,33 @@ export const CartContextProvider = ({ children }) => {
   console.log("cart", cart);
 
   const addItemToCart = (newItem) => {
-    const existingItem = cart.find((item) => item.id === newItem.id);
+    const existingItem = cart.find((item) => item._id === newItem.id);
 
     if (existingItem) {
-      // If the item already exists, increment the quantity
       existingItem.quantity += 1;
     } else {
-      // If the item does not exist, add the new item to the array
       setCart([...cart, newItem]);
     }
   };
 
   const increaseQuamtity = (id) => {
-    const index = cart.findIndex((item) => item.id === id);
+    const index = cart.findIndex((item) => item._id === id);
 
     if (index !== -1) {
-      // If the item already exists, update the quantity
       cart[index].quantity = cart[index].quantity + 1;
     }
   };
 
   const decreaseQuamtity = (id) => {
-    const index = cart.findIndex((item) => item.id === id);
+    const index = cart.findIndex((item) => item._id === id);
 
     if (index !== -1) {
-      // If the item already exists, update the quantity
       cart[index].quantity = cart[index].quantity - 1;
     }
   };
 
   const removeItemFromCart = (id) => {
-    setCart(cart.filter((item) => item.id !== id));
+    setCart(cart.filter((item) => item._id !== id));
   };
 
   return (
