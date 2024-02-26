853a051f44ef0408ca8a246342c5cebc68f99b56
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-26 12:33:13 -0800

    fix: removed category, changed name to firstname and email to emailAddress

--- client/watches/src/validations/BillingAddressValidation.js
+++ client/watches/src/validations/BillingAddressValidation.js
@@ -1,15 +1,14 @@
 import * as yup from "yup";
 
 export const BillingAddressSchema = yup.object().shape({
-  name: yup.string().required(),
+  firstname: yup.string().required(),
   lastname: yup.string().required(),
-  category: yup.string().required(),
   phone: yup.string().required(),
-  email: yup.string().required().email(),
+  emailaddress: yup.string().required().email(),
   country: yup.string().required(),
   streetaddress: yup.string().required(),
   apartment: yup.string(),
   city: yup.string().required(),
   province: yup.string().required(),
-  postcode: yup.string().required(),
+  postalcode: yup.string().required(),
 });
