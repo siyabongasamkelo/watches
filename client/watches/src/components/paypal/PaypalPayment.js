6391fd840993cf9b38322c976858495624b82ab2
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-27 12:41:22 -0800

    feat: created saveOrderData function to save order details after payment

--- client/watches/src/components/paypal/PaypalPayment.js
+++ client/watches/src/components/paypal/PaypalPayment.js
@@ -1,7 +1,44 @@
 import { PayPalButtons } from "@paypal/react-paypal-js";
+import { useContext } from "react";
+import { CheckOutContext } from "../../context/CheckOutContext";
+import { baseUrl, postRequest } from "../../utils/Services";
+import { toast } from "react-toastify";
+
 const serverUrl = "http://localhost:5000";
 
-const PaypalPayment = ({ cart, total }) => {
+const showToastErrorMessage = (message) => {
+  toast.error(message);
+};
+
+const successToastMessage = (message) => {
+  toast.success(message);
+};
+
+const PaypalPayment = ({ cart, total, saveOrder }) => {
+  const { userData, billingAddress, shippingAddress } =
+    useContext(CheckOutContext);
+
+  const saveOrderData = async (data) => {
+    try {
+      const orderData = {
+        orderId: data.orderID,
+        buyerDetails: userData,
+        billingAddress,
+        shippingAddress,
+        items: cart,
+        total,
+        status: "pending",
+      };
+
+      const saveOrder = await postRequest(`${baseUrl}/order/create`, orderData);
+      console.log(saveOrder);
+      successToastMessage(saveOrder?.data?.data);
+    } catch (err) {
+      console.log(err);
+      showToastErrorMessage("Something went wrong please try again later");
+    }
+  };
+
   let orderId;
   const createOrder = async (data) => {
     // Order is created on the server and the order id is returned
@@ -31,7 +68,13 @@ const PaypalPayment = ({ cart, total }) => {
       body: JSON.stringify({
         orderID: data.orderID,
       }),
-    }).then((response) => response.json());
+    })
+      .then((response) => response.json())
+      .then(() => {
+        saveOrderData(data);
+      });
+
+    //my fuction for saving the order on the database
   };
 
   return (
