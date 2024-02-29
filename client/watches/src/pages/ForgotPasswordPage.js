7385c7df6f0236c671c4a43219800a4987d89275
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-29 14:45:36 -0800

    feat: forgotpassword page

---
+++ client/watches/src/pages/ForgotPasswordPage.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import ForgotPassword from "../components/forgotPassword/ForgotPassword";
+import Footer from "../components/footer/Footer";
+
+const ForgotPasswordPage = () => {
+  return (
+    <Container>
+      <ForgotPassword />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ForgotPasswordPage;
