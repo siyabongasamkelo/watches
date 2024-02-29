a6ee59bb23057742e1b59c6a5d5c185fe0763345
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-29 14:46:07 -0800

    feat: resetPassword page

---
+++ client/watches/src/pages/ResetPasswordPage.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import ResetPassword from "../components/resetPassword/ResetPassword";
+import Footer from "../components/footer/Footer";
+
+const ResetPasswordPage = () => {
+  return (
+    <Container>
+      <ResetPassword />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ResetPasswordPage;
