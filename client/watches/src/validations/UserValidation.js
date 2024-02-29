327602b38e845e327cbf29eabee0a9c2859162bd
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-29 14:40:44 -0800

    feat: added forgotPassword & resetPassword Schecma

--- client/watches/src/validations/UserValidation.js
+++ client/watches/src/validations/UserValidation.js
@@ -10,3 +10,11 @@ export const LoginSchema = yup.object().shape({
   email: yup.string().email("email not valid").required(),
   password: yup.string().min(3).max(14).required(),
 });
+
+export const forgotPasswordSchema = yup.object().shape({
+  email: yup.string().email("email not valid").required(),
+});
+
+export const resetPasswordSchema = yup.object().shape({
+  password: yup.string().min(3).max(14).required(),
+});
