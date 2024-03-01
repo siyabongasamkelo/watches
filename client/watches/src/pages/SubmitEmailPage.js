a8a6725c388783540c440bfb889aafbd514d0b5e
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-29 16:18:48 -0800

    feat: submitEmail page

---
+++ client/watches/src/pages/SubmitEmailPage.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import SubmitEmail from "../components/submitEmail/SubmitEmail";
+import Footer from "../components/footer/Footer";
+
+const SubmitEmailPage = () => {
+  return (
+    <Container>
+      <SubmitEmail />
+      <Footer />
+    </Container>
+  );
+};
+
+export default SubmitEmailPage;
