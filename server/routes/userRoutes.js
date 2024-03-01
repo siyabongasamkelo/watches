60ca2e9a42b844d382303fcd75bbd67f5ae23d5d
siyabonga <siyabongasamkelociam@gmail.com>
2024-02-29 16:17:40 -0800

    fix: adde confirmemail route & fixed register route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { registerUser } from "../controllers/userController.js";
+import { confirmEmail, registerUser } from "../controllers/userController.js";
 import { loginUser } from "../controllers/userController.js";
 import { getUser } from "../controllers/userController.js";
 import { getAllUsers } from "../controllers/userController.js";
@@ -9,12 +9,13 @@ import { updateUser } from "../controllers/userController.js";
 import { forgotPassword } from "../controllers/userController.js";
 import { resetPassword } from "../controllers/userController.js";
 
-router.post("/register", registerUser);
+router.post("/register/:email/:token", registerUser);
 router.post("/login", loginUser);
 router.get("/get/:userId", getUser);
 router.get("/get", getAllUsers);
 router.delete("/delete/:userId", deleteUser);
 router.put("/update/:userId", updateUser);
+router.post("/confirmemail", confirmEmail);
 router.post("/forgotpassword", forgotPassword);
 router.post("/resetpassword/:userId/:token", resetPassword);
 
