1f6eba6f00ff96389ae77d5b5c91c5c9885d367e
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-02 10:58:33 -0800

    fix: moved styles to different file & fetched reviews & callculated rating

--- client/watches/src/components/ProductCard.js
+++ client/watches/src/components/ProductCard.js
@@ -1,95 +1,66 @@
 import { Stack } from "react-bootstrap";
-import styled from "styled-components";
-import { BagFill } from "react-bootstrap-icons";
-import { currencyFormatter } from "../utils/Services";
-import { useNavigate } from "react-router-dom";
+import { BagFill, StarFill } from "react-bootstrap-icons";
+import { baseUrl, currencyFormatter, getRequest } from "../utils/Services";
+import { useNavigate, Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import { toast } from "react-toastify";
-import { useContext } from "react";
-
-export const ProductCardStyled = styled.div`
-  height: 35vh;
-  width: 43%;
-  margin-left: 4%;
-  margin-top: 10%;
-  cursor: pointer;
-  @media only screen and (min-width: 992px) {
-    height: 40vh;
-    width: ${(props) => (props.isSpaceSmall ? "28%" : "20%")};
-    margin-top: ${(props) => (props.isSpaceSmall ? "7%" : "4%")};
-  }
-`;
-
-export const ProductImage = styled.div`
-  height: 70%;
-  width: 100%;
-  img {
-    height: 100%;
-    width: 100%;
-    border-radius: 20px;
-    object-fit: cover;
-  }
-`;
-
-export const ProductCategory = styled.p`
-  color: ${(props) => props.theme.light.smallText};
-  text-align: left;
-  padding-top: 15px;
-  font-size: 13px;
-  @media only screen and (min-width: 992px) {
-    text-align: left;
-    font-size: 17px;
-  }
-  @media only screen and (min-width: 1200px) {
-    text-align: left;
-    font-size: 14px;
-    padding-top: 10px;
-  }
-`;
-
-export const ProductName = styled.h3`
-  color: rgba(0, 0, 0, 0.7);
-  font-weight: 400;
-  font-size: 18px;
-  margin-top: -10px;
-  @media only screen and (min-width: 1200px) {
-    font-weight: 700;
-    font-size: 20px;
-  }
-`;
-
-export const ProductPrice = styled.h3`
-  color: rgba(0, 0, 0, 0.7);
-  font-weight: 400;
-  font-size: 16px;
-  @media only screen and (min-width: 1200px) {
-    font-weight: 700;
-    font-size: 16px;
-  }
-`;
-
-export const MyButton = styled.button`
-  height: 25px;
-  background-color: ${(props) => props.theme.light.secondary};
-  border-radius: 30px;
-  border: 1px solid ${(props) => props.theme.light.secondary};
-  transition: 0.5s ease-in-out;
-  font-size: 12px;
-  &:hover {
-    color: ${(props) => props.theme.light.primary};
-  }
-  @media only screen and (min-width: 992px) {
-    font-size: 15px;
-    height: 32px;
-  }
-`;
+import { useContext, useEffect } from "react";
+import { useQuery } from "react-query";
+import {
+  MyButton,
+  ProductCardStyled,
+  ProductCategory,
+  ProductImage,
+  ProductName,
+  ProductPrice,
+  ProductRating,
+} from "./ProductCard.styled";
 
 const successToastMessage = (message) => {
   toast.success(message);
 };
 
+const showToastErrorMessage = (message) => {
+  toast.error(message);
+};
+
+const calculateAverageRating = (reviews) => {
+  if (!reviews || reviews.length === 0) {
+    return 0; // Return 0 if there are no reviews
+  }
+
+  const totalRating = reviews.reduce(
+    (accumulator, review) => accumulator + review?.rating,
+    0
+  );
+  const averageRating = totalRating / reviews.length;
+  return averageRating;
+};
+
 const ProductCard = ({ item, isSpaceSmall }) => {
   const { addItemToCart } = useContext(CartContext);
+
+  const scrollToReviews = "reviews";
+  const scrollToNone = "none";
+
+  const fetchReviews = async () => {
+    try {
+      const reviews = await getRequest(`${baseUrl}/review/get/${item?._id}`);
+
+      const averageRating = calculateAverageRating(reviews?.data);
+
+      return { averageRating, totalReviews: reviews?.data?.length };
+    } catch (err) {
+      console.log(err);
+      showToastErrorMessage("there was a problem while fetching items");
+    }
+  };
+  const queryKey = ["reviews", { item }];
+  const { data, refetch } = useQuery(queryKey, fetchReviews);
+
+  useEffect(() => {
+    refetch();
+  }, [item, refetch]);
 
   const addTheItemToCart = () => {
     const newItem = { ...item, quantity: 1 };
@@ -99,20 +70,36 @@ const ProductCard = ({ item, isSpaceSmall }) => {
 
   const navigate = useNavigate();
   const goToPreviewItem = () => {
-    navigate(`/shop/${item?._id}`);
+    navigate(`/shop/${item?._id}/${scrollToNone}`);
   };
   const formattedAmount = currencyFormatter.format(item?.price);
 
   return (
-    <ProductCardStyled
-      isSpaceSmall={isSpaceSmall}
-      onClick={() => goToPreviewItem()}
-    >
-      <ProductImage>
+    <ProductCardStyled isSpaceSmall={isSpaceSmall}>
+      <ProductImage
+        onClick={() => {
+          goToPreviewItem();
+        }}
+      >
         <img src={item?.image} alt="product" />
       </ProductImage>
       <ProductCategory>{item?.category}</ProductCategory>
       <ProductName>{item?.name}</ProductName>
+      <Stack
+        direction="horizontal"
+        className=" d-flex justify-content-between align-items-center"
+      >
+        <ProductRating>
+          {data?.averageRating}
+          <Link to={`/shop/${item?._id}/${scrollToReviews}`}>
+            <StarFill style={{ margin: "0 5px 0 5px", fill: "gold" }} />
+            {data?.totalReviews} total reviews
+          </Link>
+        </ProductRating>
+
+        <div></div>
+      </Stack>
+
       <Stack
         direction="horizontal"
         className=" d-flex justify-content-between align-items-center"
