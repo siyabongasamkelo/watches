abea86acf28dfd5ce1ab95784e7987f5e070f3c4
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-04 11:22:33 -0800

    fix: fixing  reviews routes

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -6,10 +6,12 @@ import {
   getReview,
   updateReview,
   deleteReview,
+  getReviewByItemId,
 } from "../controllers/reviewController.js";
 
 router.post("/create", createReview);
-router.get("/get/:itemId", getReview);
+router.get("/get", getReview);
+router.get("/get/:itemId", getReviewByItemId);
 router.put("/update/:reviewId", updateReview);
 router.delete("/delete/:reviewId", deleteReview);
 
