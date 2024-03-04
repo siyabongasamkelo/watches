7b61086d7e7179628a18fd87a4954361640b291f
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-04 11:21:53 -0800

    feat: filtering & sorting system

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -7,6 +7,8 @@ const createReview = async (req, res) => {
     if (!review || !itemId || !rating || !userId)
       return res.status(400).json("all feilds are required");
 
+    console.log(userId);
+
     const newReview = new reviewModel({
       review,
       rating,
@@ -24,10 +26,44 @@ const createReview = async (req, res) => {
 
 const getReview = async (req, res) => {
   try {
+    const itemId = req.query.itemId;
+    const searchTerm = req.query.search || "";
+    const specificRatings = parseInt(req.query.rating) || null; // replace with the specific ratings you want to filter by
+    const sort = parseInt(req.query.sort) || 1;
+
+    const pageNumber = parseInt(req.query.page) - 1 || 1; // replace with the desired page number
+    const pageSize = 7; // replace with the desired page size
+
+    let ratingFilter = {}; // default to an empty object
+    if (specificRatings !== null) {
+      ratingFilter = { rating: specificRatings };
+    }
+
+    const reviews = await reviewModel
+      .find({
+        itemId: itemId,
+        review: { $regex: searchTerm, $options: "i" },
+        ...ratingFilter,
+      })
+      .sort({ rating: sort }) // 1 for ascending, -1 for descending
+      .skip((pageNumber - 1) * pageSize)
+      .limit(pageSize)
+      .populate("userId");
+
+    res.status(200).json(reviews);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+};
+
+const getReviewByItemId = async (req, res) => {
+  try {
     const { itemId } = req.params;
 
-    const reviews = await reviewModel.find({ itemId }).populate("userId");
-    res.status(200).json(reviews);
+    const review = await reviewModel.find({ itemId });
+
+    res.status(200).json(review);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
@@ -64,4 +100,10 @@ const deleteReview = async (req, res) => {
   }
 };
 
-export { createReview, getReview, updateReview, deleteReview };
+export {
+  createReview,
+  getReview,
+  updateReview,
+  deleteReview,
+  getReviewByItemId,
+};
