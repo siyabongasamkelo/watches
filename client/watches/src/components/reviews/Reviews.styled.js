3dfea2fff77ce42e1980a8835cd976211b019675
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-04 11:19:55 -0800

    feat: making reviews responsive

--- client/watches/src/components/reviews/Reviews.styled.js
+++ client/watches/src/components/reviews/Reviews.styled.js
@@ -4,4 +4,10 @@ export const ReviewsStyled = styled.div`
   /* height: 30vh; */
 `;
 
-export const ReviewCover = styled.div``;
+export const ReviewCover = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media only screen and (min-width: 992px) {
+    flex-direction: row;
+  }
+`;
