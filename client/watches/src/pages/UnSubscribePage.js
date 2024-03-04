e96f54b4e09558679e6675f2a1b5cf3df0fc59d7
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-04 13:57:13 -0800

    feat: unscribe page

---
+++ client/watches/src/pages/UnSubscribePage.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import UnSubscribe from "../components/unSubscribe/UnSubscribe";
+import Footer from "../components/footer/Footer";
+
+const UnSubscribePage = () => {
+  return (
+    <Container>
+      <UnSubscribe />
+      <Footer />
+    </Container>
+  );
+};
+
+export default UnSubscribePage;
