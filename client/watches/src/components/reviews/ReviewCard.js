6f41829b9b73b4d894797265ab1198c042af2d4c
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-04 11:15:58 -0800

    feat: displayed star  rating

--- client/watches/src/components/reviews/ReviewCard.js
+++ client/watches/src/components/reviews/ReviewCard.js
@@ -6,6 +6,28 @@ import {
 } from "./ReviewCard.styled";
 import propic from "../../assets/images/propic5.jpg";
 import { PreviewParagraph } from "../previewItem/PreviewItemStyled";
+import { StarFill, Star } from "react-bootstrap-icons";
+
+const StarRating = ({ rating }) => {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars.push(
+        <span key={i}>
+          <StarFill style={{ fill: "gold" }} />
+        </span>
+      ); // Full star
+    } else {
+      stars.push(
+        <span key={i}>
+          <Star />
+        </span>
+      ); // Empty star
+    }
+  }
+
+  return <div style={{ marginTop: "-30px", marginLeft: "-12px" }}>{stars}</div>;
+};
 
 const ReviewCard = ({ reviews }) => {
   return (
@@ -18,6 +40,7 @@ const ReviewCard = ({ reviews }) => {
           <PreviewParagraph>
             <strong>{reviews?.userId?.username}</strong>
           </PreviewParagraph>
+          <StarRating rating={reviews?.rating} />
           <ReviewText>{reviews?.review}</ReviewText>
         </div>
       </Review>
